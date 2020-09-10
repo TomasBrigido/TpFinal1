5b23191a6c83271fc078f011e6ab8f36f1c5649f
OctavioSosa <ocktavio123@gmail.com>
2020-09-09 22:15:55 -0300

    Agregado de comentarios en Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.sql.Timestamp;
-
+/*La clase Main se encarga de crear los hilos que disparan las transiciones y gestionan el monitor
+y los objetos más importantes del programa: red (Red de petri), c1 (colas), p1 (políticas), m (monitor)*/
 
 public class Main {
   
@@ -20,24 +21,24 @@ public class Main {
         long start=System.currentTimeMillis();
         int indice=0;
         for(int i = 0; i<17 ; i++){
-            int[] a= {i};
+            int[] a= {i};               //Creamos un arreglo para cada hilo para indicarle la transicion/es que dispara
             if(i==5){
-                a= new int[]{i, 9};
+                a= new int[]{i, 9};     //Este hilo dispara dos transiciones: la 5 y la 9
             }else if(i==6){
-                a= new int[]{i,10};
+                a= new int[]{i,10};     //Este hilo dispara dos transiciones: la 6 y la 10
             }else if(i==10 || i==9){
                 continue;
             }
-            Tarea ti = new Tarea(m,a,numeroDeTransiciones);
-            Thread hi = new Thread(ti);
-            thread_task[indice]=hi;
+            Tarea ti = new Tarea(m,a,numeroDeTransiciones); //crea la tarea (le pasamos el arreglo de transiciones)
+            Thread hi = new Thread(ti);                     //Crea el hilo, le pasa la tarea
+            thread_task[indice]=hi;                         //Colocamos los hilos en un arreglo
             hi.start();
             indice++;
         }
 
         for (int i=0;i<15;i++) {
             try {
-                thread_task[i].join();
+                thread_task[i].join();                  //Hacemos un join para la finalizacion del programa
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -45,8 +46,8 @@ public class Main {
 
         long fin = System.currentTimeMillis();
         float res=(float)(fin-start)/1000;
-        System.out.println("Tiempo total: "+res+"\n");
+        System.out.println("Tiempo total: "+res+"\n");  //Mostramos tiempo de ejecucion del programa
 
-        Logger.printBalanceEnTxt();
+        Logger.printBalanceEnTxt();     //Imprimimos las estadísticas de cada procesador y memoria.
     }
 }
